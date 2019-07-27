822b17bab2a76e5cb7d6adc3257d865686d0e4bf
Erick Baron <erickbaron123456@gmail.com>
2019-07-26 21:22:57 -0300

    Initial commit

---
+++ Controllers/CidadeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trabalho_das_Férias.Controllers
+{
+    public class CidadeController : Controller
+    {
+        // GET: Cidade
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/EstadoController.cs
@@ -0,0 +1,30 @@
+﻿using Model;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trabalho_das_Férias.Controllers
+{
+    public class EstadoController : Controller
+    {
+        // GET: Estado
+        EstadoRepository repository = new EstadoRepository();
+        public ActionResult Index()
+        {
+            List<Estado> estados = repository.ObterTodos("");
+            ViewBag.Estados = estados;
+            return View();
+        }
+        public ActionResult Cadastro(string nome, string sigla)
+        {
+            Estado estado = new Estado();
+            estado.Nome = nome;
+            estado.Sigla = sigla;
+            int id = repository.Inserir(estado);
+            return RedirectToAction("Index");
+        }
+    }
+}
